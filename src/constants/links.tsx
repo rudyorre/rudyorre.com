c02d50f3e8f17985a5f732c6ed5e449807206fd9
rudyorre <rudyorre@gmail.com>
2023-12-24 23:27:19 -0800

    Chore: Create next/ts/tailwind app

--- src/constants/links.tsx
+++
@@ -1,9 +0,0 @@
-export const links = {
-    portfolio: 'https://rudyorre.com',
-    email: 'rudyorre@ucla.edu',
-    phone: '310-505-6816',
-    github: 'https://github.com/rudyorre',
-    linkedin: 'https://linkedin.com/in/rudyorre',
-    instagram: 'https://instagram.com/rudyorre',
-    googledev: 'https://g.dev/rudy',
-};
--- src/pages/api/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from '../../layout/Layout';
-
-const API = () => {
-    <Layout>
-        <h1>Hello</h1>
-    </Layout>
-}
-
-export default API;
