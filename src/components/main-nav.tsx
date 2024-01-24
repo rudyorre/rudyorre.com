b4d2cd111c884577969bd823ae21053d17ab6d61
rudyorre <rudyorre@gmail.com>
2024-01-23 22:17:10 -0800

    feat: Smooth scroll to top
    - When clicking rudyorre.com logo

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link"
 import { usePathname } from 'next/navigation'
 import { cn } from "@/lib/utils"
+import { Button } from "./ui/button";
 
 const anchors: {[pathname: string]: string[]} = {
   "/": [
@@ -12,17 +13,31 @@ const anchors: {[pathname: string]: string[]} = {
   "/projects": [],
 };
 
+const isBrowser = () => typeof window !== 'undefined';
+const scrollToTop = () => {
+  if (!isBrowser()) return;
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
   const pathname: string = usePathname();
-  console.log({pathname});
   return (
     <nav
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
       {...props}
     >
+      {pathname === "/" ?
+        <button onClick={scrollToTop} className="inline-flex">
+          <div className="font-extrabold text-primary">rudyorre</div>.com
+        </button>
+      :
+        <Link href="/" className="inline-flex">
+          <div className="font-extrabold text-primary">rudyorre</div>.com
+        </Link>
+      }
       {pathname in anchors ? anchors[pathname].map((link, i) => 
         <a
           href={`/#${link}`}
